3529c1fa622fc2a114ac91506c113a143071575a
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-05-02 09:10:37 +0530

    add functionlity of count down

--- assets/js/app.js
+++ assets/js/app.js
@@ -93,3 +93,36 @@ menuIcon.addEventListener("click", () => {
         body.style.overflow = "auto";
     }
 })
+
+
+// countdown----------------------------------
+
+const targetDate = new Date();
+targetDate.setDate(targetDate.getDate() + 19);
+targetDate.setHours(targetDate.getHours() + 10);
+targetDate.setMinutes(targetDate.getMinutes() + 2);
+targetDate.setSeconds(targetDate.getSeconds() + 55);
+
+function updateCountdown() {
+    const now = new Date();
+    const timeLeft = targetDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    const countdownElement = document.getElementById('countdown');
+    countdownElement.textContent = `${days}d:${hours}hr:${minutes}min:${seconds}sec`;
+
+    if (timeLeft < 0) {
+        clearInterval(timerInterval);
+        countdownElement.textContent = "Countdown is over!";
+    }
+}
+
+const timerInterval = setInterval(updateCountdown, 1000);
+
+updateCountdown();
+
+
